a743efaba12e3d22fe21cf178b074507d9d90de4
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2020-12-25 19:57:24 +0800

    platform: xiaomi: Introduce device name reader
    state: platform: xiaomi: Don't read again after fails (c4be987f6004a4e66677b6a6d57e11f1251cf7b9)

---
+++ drivers/platform/xiaomi/xiaomi_series.c
@@ -0,0 +1,50 @@
+#include <linux/of.h>
+
+#include <linux/xiaomi_series.h>
+
+int xiaomi_series = XIAOMI_SERIES_MAX;
+
+int xiaomi_series_get(void)
+{
+	int ret = 0;
+	const char *xiaomi_series_str;
+
+	ret = of_property_read_string(of_find_node_by_path("/"),"xiaomi,series", &xiaomi_series_str);
+
+	if (!ret)
+		printk("Xiaomi series = %s\n",xiaomi_series_str);
+	else {
+		printk("error reading xiaomi series from FDT, ret=%d\n",ret);
+		ret = XIAOMI_SERIES_UNKNOWN;
+		goto exit;
+	}
+
+	ret = 0;
+
+	if (strcmp(xiaomi_series_str, "ulysse") == 0)
+		ret = XIAOMI_SERIES_ULYSSE;
+	else if (strcmp(xiaomi_series_str, "rova") == 0)
+		ret = XIAOMI_SERIES_ROVA;
+	else if (strcmp(xiaomi_series_str, "landtoni") == 0)
+		ret = XIAOMI_SERIES_LANDTONI;
+	else
+		ret = XIAOMI_SERIES_UNKNOWN;
+
+exit:
+	xiaomi_series = ret;
+
+	return ret;
+}
+
+int xiaomi_series_read(void)
+{
+	int ret = 0;
+
+	if (xiaomi_series == XIAOMI_SERIES_MAX)
+		ret = xiaomi_series_get();
+	else
+		ret = xiaomi_series;
+
+	return ret;
+}
+EXPORT_SYMBOL(xiaomi_series_read);
