b1ecb3a00a2235ecc107ca89305761e7d3ab0849
MrArtemSid <artem.sid.key@gmail.com>
2021-08-26 15:39:32 +0400

    input: touchscreen: HX852xES: Update for 4.19

--- drivers/input/touchscreen/HX852xES/himax_platform.c
+++ drivers/input/touchscreen/HX852xES/himax_platform.c
@@ -39,7 +39,7 @@ extern void himax_log_touch_int_devation(int touched);
 static int reg_set_optimum_mode_check(struct regulator *reg, int load_uA)
 {
 	return (regulator_count_voltages(reg) > 0) ?
-		regulator_set_optimum_mode(reg, load_uA) : 0;
+		regulator_set_load(reg, load_uA) : 0;
 }
 
 int himax_dev_set(struct himax_ts_data *ts)
